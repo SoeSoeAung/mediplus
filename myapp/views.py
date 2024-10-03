ad5dfe65a6d29e166230171587bbbde251048da8
ASUS <mobilemtk121@gmail.com>
2024-10-03 10:54:39 +0630

    home page

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def Home(request):
+    home = Home.objects.all().order_by('-id')
+     
+    return render(request, 'index.html', {'homes': homes})
