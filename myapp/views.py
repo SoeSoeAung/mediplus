c8169211633a31f50bcf97cef37646743610de4c
ASUS <mobilemtk121@gmail.com>
2024-10-08 10:19:35 +0630

    header page

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from .models import *
 
 # Create your views here.
 
-def Home(request):
+def home(request):
     home = Home.objects.all().order_by('-id')
      
-    return render(request, 'index.html', {'homes': homes})
+    return render(request, 'index.html', {'home': home})
